e2d429c0ab47539b6bcbebd2effc7c53a96c07fe
Amisha Kumari <amishakumari544@gmail.com>
2021-09-17 15:08:02 +0530

    created digital clock

--- JavaScript/001/index.js
+++ JavaScript/001/index.js
@@ -10,9 +10,11 @@ function saveBookmarkApp(e){
         url : link,
     }
     // save in the local strorage
-    localStorage.setItem('test','hello')
-    console.log(localStorage.getItem('test'))
-    console.log(bookmark);
+    // localStorage.setItem('test','hello')
+    // console.log(localStorage.getItem('test'))
+    // localStorage.removeItem('test','hello')
+    // console.log(localStorage.getItem('test'))
+    // console.log(bookmark);
     e.preventDefault();
 console.log("its working");
 
@@ -20,6 +22,15 @@ console.log("its working");
     {
         var bookmarks = [];
         bookmarks.push(bookmark)
+        // set to localstorage
+        // localStorage.setItem('bookmarks',bookmarks) // it is a json array we need to save in a string //o/p [object object]
+        localStorage.setItem('bookmarks',JSON.stringify(bookmarks)) // we need to save in a string
+        
+    }
+    else{
+        // get bookmark from local storage
+        // json.parse return string into json
+        let book = JSON.parse(localStorage.getItem('bookmarks'))
     }
 
 }
---
+++ digitalClock/Readme.md
@@ -0,0 +1,10 @@
+# Digital Clock
+
+ In this project, I learned about some basics 
+
+- querySelector - method returns the first element that matches a specified CSS selector(s) in the document.
+     we can also use document.getElementByID instead of querySelector()
+- querySelectorAll( ) will return  all the elements that matches specified Css selector  
+- setInterval() - method calls a function or evaluates an expression at specified intervals (in milliseconds). |
+    here we are using only setInterval because we don't need to stop the clock rotation if we want to make it stop we can |
+    use clearInterval() which will make the clock stop if we give a particular time in milliseconds.
